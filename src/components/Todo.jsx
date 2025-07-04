9bcc8aaf279d4669863f0ac477fb4ecda7f89947
Jallen Portugal <95646179+Liebe3@users.noreply.github.com>
2025-07-04 19:44:16 +0800

    done with edit

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,18 +1,42 @@
-import React, { useState } from "react";
+import React, { use, useEffect, useRef, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const Todo = () => {
   const [item, setItem] = useState("");
   const [listItem, setListItem] = useState([]);
+  const [error, setError] = useState("");
+
+  const [isEditing, setIsEditing] = useState("");
+  const [newItem, setNewItem] = useState("");
+
+  const inputRef = useRef();
+  const lastItemRef = useRef();
+  const editRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    if (editRef.current) {
+      editRef.current.focus();
+    }
+  }, [isEditing]);
+
+  useEffect(() => {
+    if (lastItemRef.current) {
+      lastItemRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [listItem]);
 
   const handleAddItem = () => {
-    setListItem([...listItem, { id: uuidv4(), name: item }]);
-    setItem("");
-  };
-
-  const handleEnterKey = (event) => {
-    if (event.key === "Enter") {
-      handleAddItem();
+    if (item) {
+      setListItem([...listItem, { id: uuidv4(), name: item }]);
+      setItem("");
+      setError("");
+    } else {
+      setError("Field must not be empty");
+      inputRef.current.focus();
     }
   };
 
@@ -22,10 +46,33 @@ const Todo = () => {
     );
     setListItem(filteredItem);
   };
+
+  const handleEditItem = (editId, currentName) => {
+    setNewItem(currentName);
+    setIsEditing(editId);
+  };
+
+  const handleClearLists = () => {
+    setListItem([]);
+  };
+
+  const handleEnterKey = (event) => {
+    if (event.key === "Enter") {
+      handleAddItem();
+    }
+  };
+
   return (
-    <div className="flex flex-col max-w-lg bg-green-400 min-h-screen  pt-10 items-center m-auto">
+    <div
+      onClick={(event) => {
+        if (event.target === event.currentTarget) {
+          inputRef.current.focus();
+        }
+      }} // to make sure to focus to input if the user click on white space
+      className="flex flex-col max-w-lg bg-green-400 min-h-screen  pt-10 items-center m-auto"
+    >
       <h1 className="text-2xl font-bold mb-20">MY TODO LIST</h1>
-      <div className="flex justify-evenly max-w-md w-full bg-sky-400 p-4">
+      <div className="flex justify-evenly max-w-md w-full bg-sky-400 p-4 relative">
         <input
           className="max-w-[300px] w-full"
           type="text"
@@ -33,22 +80,48 @@ const Todo = () => {
           placeholder="Enter item"
           onKeyDown={handleEnterKey}
           onChange={(event) => setItem(event.target.value)}
+          ref={inputRef}
         />
-        <button onClick={handleAddItem}>Add</button>
+        <button
+          onClick={handleAddItem}
+          className="bg-blue-600 py-1 px-3 cursor-pointer"
+        >
+          Add
+        </button>
+        <div className="absolute -bottom-1 left-10">
+          {error ? <p className="text-red-500">{error}</p> : null}
+        </div>
       </div>
       <ul className="flex flex-col max-w-md w-full border border-black py-2 px-6">
-        {listItem.map((myitem) => {
+        {listItem.map((myitem, index) => {
           return (
             <li
               key={myitem.id}
+              ref={index === listItem.length - 1 ? lastItemRef : null}
               className="flex justify-between items-center m-1"
             >
-              {myitem.name}
+              {isEditing === myitem.id ? (
+                <input
+                  type="text"
+                  value={newItem}
+                  onChange={(event) => setNewItem(event.target.value)}
+                  ref={editRef}
+                />
+              ) : (
+                <span>{myitem.name}</span>
+              )}
+
               <div className=" border border-blue-600 space-x-2">
-                <button className="bg-green-600 p-1 ">Edit</button>
+                <button
+                  onClick={() => handleEditItem(myitem.id, myitem.name)}
+                  className="bg-green-600 p-1 cursor-pointer"
+                >
+                  Edit
+                </button>
+
                 <button
                   onClick={() => handleDeleteItem(myitem.id)}
-                  className="bg-red-600 p-1"
+                  className="bg-red-600 p-1 cursor-pointer"
                 >
                   Delete
                 </button>
@@ -56,6 +129,14 @@ const Todo = () => {
             </li>
           );
         })}
+        {listItem.length > 0 ? (
+          <button
+            onClick={handleClearLists}
+            className="text-red-500 border border-red-500 w-[150px] m-auto"
+          >
+            Clear lists
+          </button>
+        ) : null}
       </ul>
     </div>
   );
